b6b586d515b41ee7f6b84f4f6c55b59c00932753
Rafael Cenzano <savagecoder77@gmail.com>
2024-07-09 00:39:24 -0500

    Create image lists for carousels

---
+++ pages/service/events/images.ts
@@ -0,0 +1,69 @@
+import { ImageType } from "@/components/carousel";
+
+const service_day = "/images/service_day/";
+const ysd = "/images/ysd/";
+const bmoc = "/images/bmoc/";
+const overnighter = "/images/overnighter/";
+
+export const ServiceDayImages: ImageType[] = [
+  { img: service_day + "RPI_Service_Day(3).jpg", alt: "RPI Service Day" },
+  { img: service_day + "RPI_Service_Day(2).jpg", alt: "RPI Service Day" },
+  { img: service_day + "RPI_Service_Day(1).jpg", alt: "RPI Service Day" },
+  { img: service_day + "RPI_Service_Day(4).jpg", alt: "RPI Service Day" },
+  { img: service_day + "RPI_Service_Day(5).jpg", alt: "RPI Service Day" },
+  { img: service_day + "RPI_Service_Day(6).jpg", alt: "RPI Service Day" },
+  { img: service_day + "RPI_Service_Day(7).jpg", alt: "RPI Service Day" },
+  { img: service_day + "RPI_Service_Day(8).jpg", alt: "RPI Service Day" },
+  { img: service_day + "RPI_Service_Day(9).jpg", alt: "RPI Service Day" },
+  { img: service_day + "RPI_Service_Day(10).jpg", alt: "RPI Service Day" },
+  { img: service_day + "RPI_Service_Day(11).jpg", alt: "RPI Service Day" },
+];
+
+export const YSDImages: ImageType[] = [
+  { img: ysd + "YSD(3).jpg", alt: "Youth Service Day" },
+  { img: ysd + "YSD(8).jpg", alt: "Youth Service Day" },
+  { img: ysd + "YSD(7).jpg", alt: "Youth Service Day" },
+  { img: ysd + "YSD(5).jpg", alt: "Youth Service Day" },
+  { img: ysd + "YSD(6).jpg", alt: "Youth Service Day" },
+  { img: ysd + "YSD(10).jpg", alt: "Youth Service Day" },
+  { img: ysd + "YSD(2).jpg", alt: "Youth Service Day" },
+  { img: ysd + "YSD(9).jpg", alt: "Youth Service Day" },
+  { img: ysd + "YSD(1).jpg", alt: "Youth Service Day" },
+  { img: ysd + "YSD(4).jpg", alt: "Youth Service Day" },
+];
+
+export const BMOCImages: ImageType[] = [
+  { img: bmoc + "BMOC(5).jpg", alt: "Biggest Meme on Campus" },
+  { img: bmoc + "BMOC(11).jpg", alt: "Biggest Meme on Campus" },
+  { img: bmoc + "BMOC(2).jpg", alt: "Biggest Meme on Campus" },
+  { img: bmoc + "BMOC(7).jpg", alt: "Biggest Meme on Campus" },
+  { img: bmoc + "BMOC(8).jpg", alt: "Biggest Meme on Campus" },
+  { img: bmoc + "BMOC(10).jpg", alt: "Biggest Meme on Campus" },
+  { img: bmoc + "BMOC(9).jpg", alt: "Biggest Meme on Campus" },
+  { img: bmoc + "BMOC(6).jpg", alt: "Biggest Meme on Campus" },
+  { img: bmoc + "BMOC(3).jpg", alt: "Biggest Meme on Campus" },
+  { img: bmoc + "BMOC(12).jpg", alt: "Biggest Meme on Campus" },
+  { img: bmoc + "BMOC(1).jpg", alt: "Biggest Meme on Campus" },
+  { img: bmoc + "BMOC(4).jpg", alt: "Biggest Meme on Campus" },
+];
+
+export const OvernighterImages: ImageType[] = [
+  { img: overnighter + "Overnighter(1).jpg", alt: "Overnighter" },
+  { img: overnighter + "Overnighter(2).jpg", alt: "Overnighter" },
+  { img: overnighter + "Overnighter(3).jpg", alt: "Overnighter" },
+  { img: overnighter + "Overnighter(4).jpg", alt: "Overnighter" },
+  { img: overnighter + "Overnighter(5).jpg", alt: "Overnighter" },
+  { img: overnighter + "Overnighter(6).jpg", alt: "Overnighter" },
+  { img: overnighter + "Overnighter(7).jpg", alt: "Overnighter" },
+  // { img: overnighter + "Overnighter(8).jpg", alt: "Overnighter" }, Image too large vertically
+  { img: overnighter + "Overnighter(9).jpg", alt: "Overnighter" },
+  { img: overnighter + "Overnighter(18).jpg", alt: "Overnighter" },
+  { img: overnighter + "Overnighter(11).jpg", alt: "Overnighter" },
+  { img: overnighter + "Overnighter(12).jpg", alt: "Overnighter" },
+  { img: overnighter + "Overnighter(17).jpg", alt: "Overnighter" },
+  { img: overnighter + "Overnighter(14).jpg", alt: "Overnighter" },
+  { img: overnighter + "Overnighter(13).jpg", alt: "Overnighter" },
+  { img: overnighter + "Overnighter(16).jpg", alt: "Overnighter" },
+  { img: overnighter + "Overnighter(10).jpg", alt: "Overnighter" },
+  { img: overnighter + "Overnighter(15).jpg", alt: "Overnighter" },
+];
