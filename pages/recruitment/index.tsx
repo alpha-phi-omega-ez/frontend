445c653631f8ad9a0bcaa45c3442069a9d8010ee
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2024-07-10 02:07:51 -0500

    Remove bell icon

--- pages/recruitment/index.tsx
+++ pages/recruitment/index.tsx
@@ -16,46 +16,16 @@ export default function RecruitmentPage() {
         <div className="text-center mt-8">
           <h1 className={title()}>{Semester} Recruitment Events</h1>
         </div>
-        <div className="mt-10 mb-4 sm:w-10/12 mx-auto">
-          {Events.map(
-            (item, index) =>
-              index % 2 === 0 && (
-                <div key={index}>
-                  <div className="mt-6 grid sm:grid-cols-2">
-                    <div>
-                      <Bell
-                        className="mx-auto justify-center main-blue-background rounded-full p-2 mb-2"
-                        size={100}
-                      />
-                    </div>
-                    <div>
-                      <h2 className="text-2xl">{item.title}</h2>
-                      <p className="mt-4">{item.description}</p>
-                      <p className="mt-4">{item.date}</p>
-                      <p>{item.time}</p>
-                      <p>{item.location}</p>
-                    </div>
-                  </div>
-                  {index + 1 < Events.length && (
-                    <div className="mt-6 grid sm:grid-cols-2">
-                      <div>
-                        <h2 className="text-2xl">{Events[index + 1].title}</h2>
-                        <p className="mt-4">{Events[index + 1].description}</p>
-                        <p className="mt-4">{Events[index + 1].date}</p>
-                        <p>{Events[index + 1].time}</p>
-                        <p>{Events[index + 1].location}</p>
-                      </div>
-                      <div>
-                        <Bell
-                          className="mx-auto justify-center main-blue-background rounded-full p-2 mb-2"
-                          size={100}
-                        />
-                      </div>
-                    </div>
-                  )}
-                </div>
-              )
-          )}
+        <div className="mt-10 mb-4 md:w-9/12 mx-auto mb-8">
+          {Events.map((item, index) => (
+            <div key={index} className="mt-8">
+              <h2 className="text-2xl font-bold">{item.title}</h2>
+              <p className="mt-4">{item.description}</p>
+              <p className="mt-4">{item.date}</p>
+              <p>{item.time}</p>
+              <p>{item.location}</p>
+            </div>
+          ))}
         </div>
       </section>
     </DefaultLayout>
