482a82a91e5c4ed78c9ffc1215681a943d946721
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2024-10-24 17:41:00 -0400

    Update LoanerTech to use a NextUI Card

--- pages/loanertech/loanertech.tsx
+++ pages/loanertech/loanertech.tsx
@@ -1,18 +1,26 @@
+import { Card, CardBody, CardHeader } from "@nextui-org/card";
 interface LoanerTechProps {
   inoffice: boolean;
   desc: string;
   id: number;
 }
 
-export default function LoanerTech({ inoffice, desc, id }: LoanerTechProps) {
+export default function LoanerTech({ desc, id, inoffice }: LoanerTechProps) {
   return (
-    <div>
-      <h2>{desc}</h2>
-      <p>{id}</p>
-      <div
-        className={`status-indicator ${inoffice ? "available" : "unavailable"}`}
-      ></div>
-      <p>{inoffice ? "Available" : "Unavailable"}</p>
-    </div>
+    <Card className="p-2">
+      <CardBody>
+        <h2 className="text-xl">
+          #{id} {desc}
+        </h2>
+        <div className="flex pt-2">
+          <div
+            className={`status-indicator ${
+              inoffice ? "available" : "unavailable"
+            }`}
+          ></div>
+          <p>{inoffice ? "Available" : "Unavailable"}</p>
+        </div>
+      </CardBody>
+    </Card>
   );
 }
