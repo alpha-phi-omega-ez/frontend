b8e88713177bc7371c8bd4847dcba243b17e84ba
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2024-10-29 13:23:58 -0400

    Update 404 page with new image

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,14 +1,20 @@
-import { title } from "@/components/primitives";
 import DefaultLayout from "@/layouts/default";
 import { Button, Link } from "@nextui-org/react";
+import { Image } from "@nextui-org/react";
 
 export default function RecruitmentPage() {
   return (
     <DefaultLayout>
       <section className="justify-center items-center h-full">
-        <div className="text-center">
+        <div className="text-center mb-10">
           <h1 className="text-9xl font-bold">404 Not Found</h1>
         </div>
+        <Image
+          width="100%"
+          alt="404 Not Found, Glump on toilet"
+          className="w-1/2 mx-auto"
+          src="/images/glump-toilet.png"
+        />
         <div className="flex justify-center mt-12 mb-8">
           <Button
             as={Link}
