126f43cb86a09a4d35306645c4f9c0605521ecdd
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2024-11-20 02:12:27 -0500

    Improve login login and handle redirects post login

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,11 +1,22 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
+import { useAuth } from "@/context/AuthContext";
 
 export default function LoginPage() {
   const router = useRouter();
+  const { auth } = useAuth();
 
   useEffect(() => {
-    router.push(`${process.env.NEXT_PUBLIC_BACKEND_SERVER}/login`);
+    const urlParams = new URLSearchParams(window.location.search);
+    const redirectPath = urlParams.get("redirect") || "/";
+
+    if (!auth.isAuthenticated) {
+      router.push(
+        `${process.env.NEXT_PUBLIC_BACKEND_SERVER}/login?redirect=${redirectPath}`
+      );
+    } else {
+      router.push("/");
+    }
   }, [router]);
 
   return null;
