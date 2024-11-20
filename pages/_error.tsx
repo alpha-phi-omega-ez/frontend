27965e8985e96f698e7ec4f7c444422e43ac872e
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2024-11-20 00:06:08 -0500

    Update error componnent

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,30 +1,27 @@
-import DefaultLayout from "@/layouts/default";
 import { Image, Button, Link } from "@nextui-org/react";
 
-export default function ErrorPage(title: string) {
+export default function Error({ title }: { title: string }) {
   return (
-    <DefaultLayout>
-      <section className="justify-center pb-4 md:pb-6">
-        <div className="text-center mb-10">
-          <h1 className="text-9xl font-bold">{title}</h1>
-        </div>
-        <Image
-          width="100%"
-          alt="Error logging in"
-          className="w-1/2 mx-auto"
-          src="/images/glump-confused.png"
-        />
-        <div className="flex justify-center mt-12 mb-8">
-          <Button
-            as={Link}
-            className="p-8 text-xl main-blue-background main-gold-color"
-            href="/"
-            variant="flat"
-          >
-            Home
-          </Button>
-        </div>
-      </section>
-    </DefaultLayout>
+    <div className="mb-10">
+      <div className="text-center mb-10">
+        <h2 className="text-3xl font-bold">{title}</h2>
+      </div>
+      <Image
+        width="100%"
+        alt="Error logging in"
+        className="w-1/3 mx-auto"
+        src="/images/glump-confused.png"
+      />
+      <div className="flex justify-center mt-12 mb-8">
+        <Button
+          as={Link}
+          className="p-8 text-xl main-blue-background main-gold-color"
+          href="/"
+          variant="flat"
+        >
+          Home
+        </Button>
+      </div>
+    </div>
   );
 }
