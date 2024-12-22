d3ed1356d83d39962e9cdc74f28451b0b097e2c4
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2024-12-22 00:05:34 -0800

    new laf view type

---
+++ types/laf.ts
@@ -0,0 +1,9 @@
+export type ViewState =
+    | "Found Item"
+    | "Lost Items"
+    | "Submit Lost Report"
+    | "Find Lost Report"
+    | "Matching Lost Reports"
+    | "Expired Items"
+    | "Archive"
+    | "error";
