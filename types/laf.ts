6da57b920eda8839bb3b85b2aff25849b41a5d43
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2024-12-26 22:34:41 -0800

    Added new LAF types

--- types/laf.ts
+++ types/laf.ts
@@ -7,3 +7,25 @@ export type ViewState =
     | "Expired Items"
     | "Archive"
     | "error";
+
+export type LAFItem = {
+    id: string;
+    type: string;
+    location: string;
+    date: string;
+    description: string;
+    found: string;
+    archived: string;
+}
+
+export type LostReportItem = {
+    id: string
+    name: string;
+    email: string;
+    type: string;
+    location: string[];
+    date: string;
+    description: string;
+    found: string;
+    archived: string;
+}
