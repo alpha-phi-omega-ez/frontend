ec7145544594f010d4c3e390d065168b43c01331
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2024-12-26 23:56:49 -0800

    Component to list LAF items in a table

---
+++ components/laf/laf-items.tsx
@@ -0,0 +1,60 @@
+import { LAFItem } from "@/types/laf";
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  getKeyValue,
+} from "@nextui-org/react";
+
+interface LAFItemsProps {
+  items: LAFItem[];
+}
+
+export default function LAFItems({ items }: LAFItemsProps) {
+  const columns = [
+    {
+      key: "id",
+      label: "ID",
+    },
+    {
+      key: "type",
+      label: "Type",
+    },
+    {
+      key: "location",
+      label: "Location",
+    },
+    {
+      key: "description",
+      label: "Description",
+    },
+    {
+      key: "date",
+      label: "Date",
+    },
+  ];
+
+  return (
+    <Table aria-label="LAF Items" className="my-5" isStriped>
+      <TableHeader columns={columns}>
+        {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
+      </TableHeader>
+      <TableBody items={items} emptyContent={"No LAf items found."}>
+        {(item) => (
+          <TableRow key={item.id}>
+            {(columnKey) => (
+              <TableCell>
+                {columnKey === "id"
+                  ? item.type.charAt(0) + item.id
+                  : getKeyValue(item, columnKey)}
+              </TableCell>
+            )}
+          </TableRow>
+        )}
+      </TableBody>
+    </Table>
+  );
+}
