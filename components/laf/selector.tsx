708d14c08cdbb0413a3f3dea81770cc58071095d
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2024-12-30 00:33:27 -0800

    cleanup selector

--- components/laf/selector.tsx
+++ components/laf/selector.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { ViewState } from "@/types/laf";
 
 interface LAFSelectorProps {
@@ -17,13 +16,13 @@ export default function LAFSelector({
   };
 
   return (
-    <div className="flex items-center justify-center mt-10 mb-10">
+    <div className="flex items-center justify-center my-10">
       <div className="flex items-center justify-around rounded-full bg-gray-400 p-2 overflow-hidden">
         {views.map((item, index) => (
           <button
             key={index}
             onClick={() => changeSelector(item)}
-            className={`flex-1 mx-1 py-2 rounded-full text-center transition-all duration-300 
+            className={`flex-1 mx-1 py-2 px-2 rounded-full text-center transition-all duration-300 
             ${
               view === item
                 ? "bg-blue-500 shadow-md text-black"
