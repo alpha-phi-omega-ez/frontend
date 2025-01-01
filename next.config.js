38208cd937dd816267c0cfa2dad34533e9ea1f58
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2025-01-01 01:37:08 -0800

    add redirects for routes from the old website

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/laf.php", // The old route you want to redirect from
+        destination: "/laf", // The new route you want to redirect to
+        permanent: true, // Use true for a 308 permanent redirect
+      },
+      {
+        source: "/home.php", // Another old route you want to redirect from
+        destination: "/", // The new route you want to redirect to
+        permanent: true, // Use true for a 308 permanent redirect
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
