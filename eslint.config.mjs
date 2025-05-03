aa33a8a00fa4a9e32ebbd2ce923797666beaa572
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2025-05-03 12:24:10 -0400

    add next for eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,5 @@
 import eslintPluginReact from "eslint-plugin-react";
+import eslintPluginNext from "@next/eslint-plugin-next";
 import typescriptEslintParser from "@typescript-eslint/parser";
 import typescriptEslintPlugin from "@typescript-eslint/eslint-plugin";
 
@@ -11,18 +12,16 @@ export default [
         ecmaVersion: 2021,
         sourceType: "module",
       },
-      ecmaFeatures: {
-        jsx: true,
-      },
     },
     plugins: {
       react: eslintPluginReact,
       "@typescript-eslint": typescriptEslintPlugin,
+      "@next/next": eslintPluginNext,
     },
     rules: {
       ...eslintPluginReact.configs.recommended.rules,
       ...typescriptEslintPlugin.configs.recommended.rules,
-
+      ...eslintPluginNext.configs.recommended.rules,
       "react/react-in-jsx-scope": "off", // React 17+ JSX transform
     },
     settings: {
@@ -30,5 +29,6 @@ export default [
         version: "detect",
       },
     },
+    ignores: ["node_modules", ".next", "dist", "build"],
   },
 ];
