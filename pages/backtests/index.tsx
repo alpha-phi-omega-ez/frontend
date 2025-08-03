7714c4c13b7c408d9e9d8810d09259e0005645d2
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2025-08-03 09:27:32 -0700

    Update to use reducer instead of multiple usestates

--- pages/backtests/index.tsx
+++ pages/backtests/index.tsx
@@ -1,32 +1,65 @@
 import { title } from "@/components/primitives";
-import { useState } from "react";
+import { useReducer } from "react";
 import CourseCodes from "@/components/backtest/coursecodes";
 import Courses from "@/components/backtest/courses";
 import Backtests from "@/components/backtest/backtests";
 import { Breadcrumbs, BreadcrumbItem, Button } from "@heroui/react";
 import Error from "@/components/error";
+import { BacktestState, ReducerActions } from "@/types/backtest";
 
 interface BacktestPageProps {
   courseCodes: string[];
 }
 
 export default function BacktestPage({ courseCodes }: BacktestPageProps) {
-  const [view, setView] = useState<"codes" | "courses" | "backtests" | "error">(
-    courseCodes ? "codes" : "error"
-  );
-  const [currentCourseCode, setCurrentCourseCode] = useState<null | string>(
-    null
-  );
-  const [currentCourse, setCurrentCourse] = useState<null | {
-    id: string;
-    name: string;
-  }>(null);
-  const [courses, setCourses] = useState<null | { id: string; name: string }[]>(
-    null
-  );
-  const [backtests, setBacktests] = useState<
-    null | { type: string; tests: string[] }[]
-  >(null);
+  function reducer(
+    state: BacktestState,
+    action: ReducerActions
+  ): BacktestState {
+    switch (action.type) {
+      case "TO_COURSES":
+        return {
+          ...state,
+          view: "courses",
+          currentCourse: null,
+          backtests: null,
+        };
+      case "TO_CODES":
+        return {
+          ...state,
+          view: "codes",
+          currentCourseCode: null,
+          currentCourse: null,
+          backtests: null,
+        };
+      case "SET_COURSES":
+        return {
+          ...state,
+          view: "courses",
+          courses: action.payload.courses,
+          currentCourseCode: action.payload.currentCourseCode,
+        };
+      case "SET_BACKTESTS":
+        return {
+          ...state,
+          view: "backtests",
+          backtests: action.payload.backtests,
+          currentCourse: action.payload.currentCourse,
+        };
+      case "ERROR":
+        return { ...state, view: "error" };
+      default:
+        return state;
+    }
+  }
+
+  const [backtestState, dispatch] = useReducer(reducer, {
+    view: courseCodes ? "codes" : "error",
+    currentCourseCode: null,
+    currentCourse: null,
+    courses: null,
+    backtests: null,
+  });
 
   return (
     <section className="justify-center pb-4 md:pb-6">
@@ -38,71 +71,56 @@ export default function BacktestPage({ courseCodes }: BacktestPageProps) {
           <BreadcrumbItem>
             <Button
               onPress={() => {
-                setView("codes");
-                setCurrentCourseCode(null);
-                setCurrentCourse(null);
-                setBacktests(null);
+                dispatch({ type: "TO_CODES" });
               }}
-              isDisabled={view === "codes"}
+              isDisabled={backtestState.view === "codes"}
             >
               Course Codes
             </Button>
           </BreadcrumbItem>
-          {currentCourseCode && (
+          {backtestState.currentCourseCode && (
             <BreadcrumbItem>
               <Button
                 onPress={() => {
-                  setView("courses");
-                  setCurrentCourse(null);
-                  setBacktests(null);
+                  dispatch({ type: "TO_COURSES" });
                 }}
-                isDisabled={view === "courses"}
+                isDisabled={backtestState.view === "courses"}
               >
-                {currentCourseCode}
+                {backtestState.currentCourseCode}
               </Button>
             </BreadcrumbItem>
           )}
-          {currentCourse && (
+          {backtestState.currentCourse && (
             <BreadcrumbItem>
-              <Button isDisabled>{currentCourse.name}</Button>
+              <Button isDisabled>{backtestState.currentCourse.name}</Button>
             </BreadcrumbItem>
           )}
         </Breadcrumbs>
       </div>
       <div
         style={{
-          display: view === "codes" ? "block" : "none",
+          display: backtestState.view === "codes" ? "block" : "none",
         }}
       >
-        <CourseCodes
-          courseCodes={courseCodes}
-          setView={setView}
-          setCurrentCourseCode={setCurrentCourseCode}
-          setCourses={setCourses}
-        />
+        <CourseCodes courseCodes={courseCodes} dispatch={dispatch} />
       </div>
       <div
         style={{
-          display: view === "courses" ? "block" : "none",
+          display: backtestState.view === "courses" ? "block" : "none",
         }}
       >
-        <Courses
-          courses={courses}
-          setCurrentCourse={setCurrentCourse}
-          setView={setView}
-          setBacktests={setBacktests}
-        />
+        <Courses courses={backtestState.courses} dispatch={dispatch} />
       </div>
       <div
         style={{
-          display: view === "backtests" ? "block" : "none",
+          display: backtestState.view === "backtests" ? "block" : "none",
         }}
       >
-        <Backtests backtests={backtests} />
+        <Backtests backtests={backtestState.backtests} />
       </div>
       <div
         style={{
-          display: view === "error" ? "block" : "none",
+          display: backtestState.view === "error" ? "block" : "none",
         }}
       >
         <Error title="Failed to load backtests" />
