fed0440a82b2b56d74ae24c8737c66a38cb20d29
SeanHohmann <sean777hohmann@gmail.com>
2026-08-22 18:12:34 -0400

    update recruitment events

--- data/recruitment.ts
+++ data/recruitment.ts
@@ -1,4 +1,4 @@
-export const Semester = "Spring 2025";
+export const Semester = "Fall 2026";
 
 interface Event {
   title: string;
@@ -10,63 +10,48 @@ interface Event {
 
 export const Events: Event[] = [
   {
-    title: "Presentation Blunders 101",
-    description: "Learn how to present in a professional setting!",
-    date: "Monday, January 13th, 2025",
-    time: "5 pm to 6:30 pm",
-    location: "Union Shellnut Gallery",
-  },
-  {
-    title: "Team Trivia",
-    description:
-      "Test your knowledge and help raise funds for food for the UN World Food Program through Free Rice",
-    date: "Wednesday January 15th, 2025",
-    time: "4 pm to 5:30 pm",
-    location: "Union Shellnut Gallery",
-  },
-  {
-    title: "USS Slater",
-    description: "Learn about and clean the USS Slater in Albany",
-    date: "Saturday, January 18th, 2025",
-    time: "TBA",
-    location: "USS Slater, Meet at the APO Office Union 3420",
-  },
-  {
-    title: "Game Night, Hot Cocoa, and Cookie Decorating",
-    description: "Join us for a night of fun and relaxation!",
-    date: "Tuesday, January 21st, 2025",
-    time: "4 pm to 6:30 pm",
-    location: "Union Shellnut Gallery",
-  },
-  {
     title: "Card Making",
+    description: "Make cards for hospitalized children",
+    date: "Monday, August 31st, 2026",
+    time: "6 pm to 7 pm",
+    location: "Union Shelnutt Gallery",
+  },
+  {
+    title: "Presentation Night",
     description:
-      "Make cards for soldiers overseas to send back to their families",
-    date: "Thursday, January 23rd, 2025",
-    time: "5:30 pm to 7 pm",
-    location: "Union Shellnut Gallery",
+      "Learn how to present in a professional setting!",
+    date: "Thursday, September 3rd, 2026",
+    time: "6 pm to 7 pm",
+    location: "Union Phalanx Room",
   },
   {
     title: "Bob Ross Night",
     description: "Paint along with Bob Ross and meet APO brothers",
-    date: "Friday, January 24th, 2025",
+    date: "Friday, September 4th, 2026",
     time: "6 pm to 9 pm",
-    location: "Union McNeil Room",
+    location: "Union Shelnutt Gallery",
   },
   {
-    title: "Resume Review",
+    title: "Board Games, Apple Cider, and Cookies",
+    description: "Join us for a night of fun and relaxation!",
+    date: "Tuesday, September 8th, 2026",
+    time: "7 pm to 9 pm",
+    location: "Low 4040",
+  },
+  {
+    title: "Dog Toy Crafting",
     description:
-      "Get your resume reviewed by APO brothers with internship/REU experience",
-    date: "Monday, January 27th, 2025",
-    time: "6:30 pm to 7:30 pm",
-    location: "Union Shellnut Gallery",
+      "Make dog toys out of old shirts",
+    date: "Thursday, September 10th, 2026",
+    time: "5 pm to 7 pm",
+    location: "Union Shelnutt Gallery",
   },
   {
     title: "Welcome Dinner",
     description:
       "Join the Members of Alpha Phi Omega, a national service fraternity in welcoming the potential new members with a delicious meal!",
-    date: "Friday, January 31st, 2025",
+    date: "Friday, September 11th, 2026",
     time: "6 pm to 9 pm",
-    location: "Union Shellnut Gallery",
+    location: "Union Shelnutt Gallery",
   },
 ];
